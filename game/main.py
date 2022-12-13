f2cca7d320793865b1031fb46a8d6102f9db057f
kike <enrique.angelo.rios@gmail.com>
2022-12-13 16:28:44 -0600

    cambios en main

--- game/main.py
+++ game/main.py
@@ -1,22 +1,80 @@
-import store
-from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
+import random
 
-app = FastAPI()
 
-@app.get('/')
-def get_list():
-    return [1,2,3,]
+def choose_options():
+  options = ('piedra', 'papel', 'tijera')
+  user_option = input('piedra, papel o tijera => ')
+  user_option = user_option.lower()
 
-@app.get('/contact', response_class=HTMLResponse)
-def get_list():
-    return """
-        <h1>Hola soy una pagina</h1>
-        <p>soy un parrafo</p>
-    """
+  if not user_option in options:
+    print('esa opcion no es valida')
+     # continue
+    return None, None
+  
+  computer_option = random.choice(options)
 
-def run():
-    store.get_categories()
+  print('User option =>', user_option)
+  print('Computer option =>', computer_option)
+  return user_option, computer_option
 
-if __name__ == '__main__':
-    run()
+def check_rules(user_option, computer_option, user_wins,
+ computer_wins):
+  if user_option == computer_option:
+    print('Empate!')
+  elif user_option == 'piedra':
+    if computer_option == 'tijera':
+      print('piedra gana a tijera')
+      print('user gano!')
+      user_wins += 1
+    else:
+      print('Papel gana a piedra')
+      print('computer gano!')
+      computer_wins += 1
+  elif user_option == 'papel':
+    if computer_option == 'piedra':
+      print('papel gana a piedra')
+      print('user gano')
+      user_wins += 1
+    else:
+      print('tijera gana a papel')
+      print('computer gano!')
+      computer_wins += 1
+  elif user_option == 'tijera':
+    if computer_option == 'papel':
+      print('tijera gana a papel')
+      print('user gano!')
+      user_wins += 1
+    else:
+      print('piedra gana a tijera')
+      print('computer gano!')
+      computer_wins += 1
+  return user_wins, computer_wins    
+
+def run_game():
+   computer_wins = 0
+   user_wins = 0
+   rounds = 1
+
+   while True:
+     print('*' * 10)
+     print('ROUND', rounds)
+     print('*' * 10)
+
+     print('computer_wins', computer_wins)
+     print('user_wins', user_wins)
+     rounds += 1
+    
+     user_option, computer_option= choose_options()
+     user_wins, computer_wins = check_rules(user_option,
+     computer_option, user_wins, computer_wins)
+
+
+     if computer_wins == 2:
+       print('El ganador es la computadora')
+       break
+
+     if user_wins == 2:
+       print('El ganador es el usuario')
+       break
+
+run_game()
